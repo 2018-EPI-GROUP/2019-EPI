0183eec9c22fbf7d8b8d8c5b339e1016d2dd69a7
Tjx-666 <56494487+Tjx-666@users.noreply.github.com>
2019-10-14 00:33:48 +0800

    Add files via upload
    smile例题

---
+++ smile书上示例.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	for (int i = 3; i>0; i--)
+	{
+		for (int j=1; j<=i; j++)
+		{
+			cout << "Smile£¡  ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
