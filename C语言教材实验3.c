c1d6b873299cf3ef5311b1bea60de71cd7084ee2
liwesi <a2806008739@gmail.com>
2019-10-18 21:33:06 +0800

    练习

---
+++ C语言教材实验3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include<math.h>
+#define PI  3.14
+/*int main()
+{
+    double r,h,c,s,S,V1,V2;
+    printf("请输入圆半径:");
+    scanf("%lf",&r);
+    printf("请输入圆柱高:");
+    scanf("%lf",&h);
+    c=2*PI*r;
+    s=PI*sqrt(r);
+    S=4*PI*sqrt(r);
+    V1=4.0/3*PI*r*sqrt(r);
+    V2=s*h;
+    printf("圆周长为:%.2lf\n",c);
+    printf("圆面积为:%.2lf\n",s);
+    printf("圆球表面积为:%.2lf\n",S);
+    printf("圆球体积为:%.2lf\n",V1);
+    printf("圆柱体积为:%.2lf\n",V2);
+    return 0;
+
+
+}*/
+/*int main()
+{
+    int a,x,y;
+    scanf("%d %d",&x,&y);
+    a=pow(x,y);
+    printf("%d",a);
+}*/
+
+/*int main()
+{   double a,b;
+    scanf("%lf",&a);
+    b=sin(a)+cos(a);
+    printf("%lf",b);
+}
+*/
+
