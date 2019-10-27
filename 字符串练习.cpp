00990a59f371eaa00c04a050c0ae29041240a173
Tjx-666 <56494487+Tjx-666@users.noreply.github.com>
2019-10-27 22:07:00 +0800

    Add files via upload

---
+++ 字符串练习.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+#include<cstring>
+int main(void)
+{
+	char a[10];
+	int i;
+	while(1)
+	{
+		i++;
+		a[i] = '0';
+	}
+	//scanf_s("%s", a,10);
+	//printf("%s", a);
+	return 0;
+}
