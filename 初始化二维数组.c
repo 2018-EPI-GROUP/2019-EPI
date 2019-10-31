022c53223eb9dc8213db4b0202b012febfb12a58
Qulonk <56723366+Qulonk@users.noreply.github.com>
2019-10-31 20:00:59 +0800

    Update 初始化二维数组.c

--- 初始化二维数组.c
+++ 初始化二维数组.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 int main(void)
 {
-	int a[4][4] = {{1}{1,2}{1,2,3}};
+	int a[4][4] = {{1},{1,2},{1,2,3},{1,2,3,4}};
 	int i , j ;
-	for(i = 1; i <= 4; i++ )
-		{for(j = 1; j <= 4; j++ )
+	for(i = 0; i <= 3; i++ )
+		{for(j = 0; j <= 3; j++ )
 			printf("a[%d][%d] %d  ", i , j, a[i][j] );
 		}
 	return 0;
