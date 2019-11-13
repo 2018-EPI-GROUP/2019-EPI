dfe90acdd6e1d8f47050e992c9b60d4b10ccff9b
Qulonk <56723366+Qulonk@users.noreply.github.com>
2019-11-13 20:49:53 +0800

    Add files via upload

---
+++ 结构体与函数.c
@@ -0,0 +1,61 @@
+#include<stdio.h>
+
+struct point{
+	
+	int a  ;
+	
+	int b  ;
+	
+	int c ;
+} ;
+
+struct point getstruct(struct point *fp1)
+{	
+	scanf("%d" , &(*fp1).a ) ;
+	
+	scanf("%d" , &(*fp1).b ) ;
+	
+	scanf("%d" , &(*fp1).c ) ;
+	
+	printf("fp1         %d-%d-%d \n" , (*fp1).a , (*fp1).b , (*fp1).c ) ;
+	
+	return  *fp1 ;
+}
+
+struct point gotstruct(void)
+{	
+	struct point temp ;
+	
+	scanf("%d" , &temp.a ) ;
+	
+	scanf("%d" , &temp.b ) ;
+	
+	scanf("%d" , &temp.c ) ;
+	
+	printf("temp        %d-%d-%d \n" , temp.a , temp.b , temp.c ) ;
+	
+	return  temp ;
+}
+
+int main(void)
+{
+	struct point p1= { 1 , 2 , 3 };
+	
+	struct point p2= { 4 , 5 , 6 };
+	
+	printf("P1(before)  %d-%d-%d \n" , p1.a , p1.b , p1.c ) ;
+	
+	printf("P2(before)  %d-%d-%d \n" , p2.a , p2.b , p2.c ) ;
+	
+	struct point *fp = &p1 ;
+	
+	getstruct(fp);              //传指针 
+	
+	p2 = gotstruct() ;          //传整个结构 
+	
+	printf("P1(after)   %d-%d-%d \n" , p1.a , p1.b , p1.c ) ;
+	
+	printf("P2(after)   %d-%d-%d \n" , p2.a , p2.b , p2.c ) ;
+	
+	return 0 ;
+}
