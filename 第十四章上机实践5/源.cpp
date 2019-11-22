4798c34f835eca994f0dc4a20a6f9ba89782c727
Adriancanfly <1351475237@qq.com>
2019-11-22 17:21:30 +0800

    向函数传递结构的成员

---
+++ 第十四章上机实践5/源.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#define FUNDLEN 50
+
+struct funds {
+	char bank[FUNDLEN];
+	double bankfund;
+	char save[FUNDLEN];
+	double savefund;
+};
+
+double sum(double, double);
+
+int main(void)
+{
+	struct funds stan = {
+		"Garlic-Melon Bank",
+		4032.27,
+		"Lucky's Savings and Loan",
+		8543.94
+	};
+
+	printf("Stan has a tatal of $%.2f.\n",
+		sum(stan.bankfund, stan.savefund));
+	return 0;
+}
+
+double sum(double x, double y)
+{
+	return(x + y);
+}
