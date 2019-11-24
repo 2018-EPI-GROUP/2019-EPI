3d1e693c4674e5cd50cb787ff6c87ac7d5cd40d8
Qulonk <56723366+Qulonk@users.noreply.github.com>
2019-11-24 20:28:20 +0800

    Add files via upload

---
+++ 二叉树/二叉树/二叉树.cpp
@@ -0,0 +1,69 @@
+﻿#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct Node Node;
+
+struct Node {
+	long int data;
+	int cnt;
+	Node* pleft;
+	Node* pright;
+};
+
+Node *pRoot = NULL;
+
+Node *create_node(long int value)                //创建根节点
+{
+	Node *pNode = (Node*)malloc(sizeof(Node));
+
+	pNode->data = value;
+
+	pNode->cnt = 1;
+
+	pNode->pleft = pNode->pright = NULL;
+
+	return pNode;
+}
+
+Node* add_Node(long int value, Node *pNode)       //添加新节点 ，返回一个插入结点的指针
+{
+	if (value == pNode->data) {
+		pNode->cnt++;
+		return pNode;
+	}
+	if (value < pNode->data) {
+		if (!pNode->pleft) {
+			pNode->pleft = create_node(value);
+			return pNode->pleft;
+		}
+		else
+			return add_Node(value, pNode->pleft);
+	}
+	else{
+		if (!pNode->pright) {
+			pNode->pright = create_node(value);
+			return pNode->pright;
+		}
+		else 
+			return add_Node(value, pNode->pright);
+	}
+}
+
+int main(void)
+{
+	int i ,newvalue = 1;
+
+	for( i = 0 ; i < 5 ; i++ ){
+		
+		scanf("%d", &newvalue);
+
+		if (!pRoot)
+			pRoot = create_node(newvalue);
+		
+		else
+			add_Node(newvalue, pRoot);
+	}
+	printf("输入成功！");
+
+	return 0;
+}
