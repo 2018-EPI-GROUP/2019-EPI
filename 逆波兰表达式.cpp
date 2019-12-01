7016d0c148390b106c47d819173bd7c6a5208ad8
Qulonk <56723366+Qulonk@users.noreply.github.com>
2019-12-01 17:46:26 +0800

    Add files via upload

---
+++ 逆波兰表达式.cpp
@@ -0,0 +1,38 @@
+#include"head.h"
+#include<stdio.h>
+#include<stdlib.h>
+
+double exp(void)
+{
+	char a[20];
+
+	scanf("%s", a);
+
+	switch (a[0])
+	{
+	case('+'):
+		return exp() + exp();
+		break;
+	case('-'):
+		return exp() - exp();
+		break;
+	case('*'):
+		return exp() * exp();
+		break;
+	case('/'):
+		return exp() / exp();
+		break;
+	default:
+		return atof(a);
+		break;
+	}
+}
+
+int main(void)
+{
+	printf("%lf\n" , exp());
+
+	system("pause");
+
+	return 0;
+}
